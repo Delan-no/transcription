162dd3cb6754f0ebf1c4a48d88c457ceced9ab1e
Délanno Kotcho <delanktc24@gmail.com>
2024-07-19 16:24:40 +0200

    suppression de certains fichier

--- transcription.py
+++ transcription.py
@@ -1,10 +1,11 @@
 from gtts import gTTS
 import os
 
-# Définir le texte à convertir en audio
-texte = "Bonjour, comment allez-vous ? je suis délanno kotcho un programmeur des applications web et mobile."
-
-# fonction pour executer la transcription
+# Ouvrir le fichier texte en mode lecture
+with open("text.txt", "r") as fichier:
+  # Lire le contenu du fichier
+  contenu = fichier.read()
+  # fonction pour executer la transcription
 
 def convertir_texte_audio(texte):
     # Créer un objet gTTS
@@ -14,5 +15,5 @@ def convertir_texte_audio(texte):
     # Lire l'audio
     os.system("audio.mp3")
     
-convertir_texte_audio(texte)
+convertir_texte_audio(contenu)
                   
