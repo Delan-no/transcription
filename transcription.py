8b725d1c95cbc09018fe37bd81e94d5cafe518b8
Délanno Kotcho <delanktc24@gmail.com>
2024-07-23 17:16:12 +0200

    encore un commiter en un comm

--- transcription.py
+++ transcription.py
@@ -5,7 +5,7 @@ import os
 # with open("text.txt", "r") as fichier:
   # Lire le contenu du fichier
   # contenu = fichier.read()
-contenu = "Programmeur dévoué avec une année d’expérience dans le développement de solutions logicielles efficaces et innovantes. J’aime ce que je fais et suis prêt à relever de nouveaux défis tout en contribuant au succès d’une équipe dynamique et en favorisant l’amélioration continue"
+contenu = "Programmeur dévoué avec une année d’expérience dans le développement de solutions logicielles efficaces et innovantes. J’aime ce que je fais et suis prêt à relever de nouveaux défis tout en contribuant au succès d’une équipe dynamique et en favorisant l’amélioration continue. j'aime trop le code. C'est ma vie les codes"
   # fonction pour executer la transcription
 
 def convertir_texte_audio(texte):
